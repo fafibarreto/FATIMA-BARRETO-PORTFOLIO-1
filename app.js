96387c0e3c70e567ad26cbd5743706ecb12e9028
fafibarreto <fatimacbeventos@gmail.com>
2021-10-27 18:05:31 +0100

    javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+let janela = document.querySelectorAll('.card-wrapper');
+let menu = document.querySelectorAll('.card-wrapper')
+
+function abrirMenu(){
+    janela.classcard-container.toggle('abrirMenu');
+    menu.classcard-container.toggle('abrirMenu');
+}
+
+menu.onclick = abrirMenu;
